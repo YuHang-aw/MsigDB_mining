b87de36078b5fb8bbc95aad835fb038a99f8f47c
YuHang-aw <69309965+YuHang-aw@users.noreply.github.com>
2024-09-16 10:50:46 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Msigdb_terms_research',
+    version='0.1',
+    packages=find_packages(),
+    description='',
+    author='Yuhang Huang',
+    url='',
+    install_requires=requirements,  
+    python_requires='>=3.8',
+)
